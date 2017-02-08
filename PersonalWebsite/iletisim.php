843a61cf7df470952a6396c97f64fc6b80c6e9e2
Cem Gunes <cemguness@live.com>
2017-02-08 14:32:50 +0100

    Add files via upload

---
+++ PersonalWebsite/iletisim.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<meta name="keywords" content="Portfolio of Cem Gunes">
+		<meta name="author" content="Cem Gunes">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+		<title>Cem Gunes | Welcome</title>
+
+		<link rel="stylesheet" type="text/css" href="./css/style.css">
+		<link rel="shortcut icon" href="./img/logo.ico">
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+
+	<script type = "text/javascript" language = "javascript" > $(document).ready(function (e) {
+	     $(".header-menu-list .btn-expand").click(function (e) {
+			$('.collapsible').toggle("slide", {
+	            direction: "right"
+	        }, 1000);
+	     });
+	 });
+	</script>
+</head>
+
+<header>
+
+<a href="index.php" title="Homepage" id="Homepage-logo"></a>
+
+
+
+<div class="header-menu-list">
+	
+	<div class="btn-expand">
+	</div>
+	
+	<div class="collapsible">
+		<ul>
+			<li><a href="mimarlik.php">Mimarlik</a></li>
+			<li><a href="fotograf.php">Fotograf</a></li>
+			<li><a href="sinema.php">Sinema</a></li>
+			<li><a href="webtasarim.php">Web Tasarim</a></li>				
+			<li><a href="seyahat.php">Seyahat</a></li>	
+			<li><a href="benkimim.php">Ben <br> Kimim?</a></li>	
+			<li><a href="iletisim.php">Iletisim</a></li>	
+		</ul>
+	</div>
+</div>
+
+</header>
+
+
+<body>
+	<div class="p1">
+		<p>
+			Filippo ile Lorenzo arasındaki rekabet bir kaç yıldan beri alttan alta kızışıyordu. 
+			İki adam eşit yetkilerle göreve atanmış olsa da, Filippo'nun parıltısı kısa zamanda Lorenzo'yu gölgede bıraktı.
+			Öküz-kaldıracının yapılması ve ilk tan hatılın yerine konulmasından sonra belgelerde ondan <i>inventor 
+			et ghubernator maior cupolae</i>diye söz edilmeye başladı...<br>
+			<i><b>Brunelleschi'nin Kubbesi- Floransa'daki Büyük Katedralin hikayesi </b><br> Ross King </i>
+		</p>
+
+	</div>
+
+		<div class="p2">
+		<p>
+			«Changer  la  ville  pour  changer  la  vie»  est  donc,  pour  Lefebvre–même s’il est partisan du changement 
+			social et spatial–synonyme d’erreur et de mystification. Il propose de prendre le problème à l’envers. C’est 
+			le changement social et dans la «vie quotidienne» qui permettra au mode de production de l’espace et à 
+			l’espace lui-même de se transformer. ...<br>
+			<i><b>L’espace politique chez Henri Lefebvre: l’idéologie et l’utopie </b><br> Grégory Busquet </i>
+		</p>
+
+	</div>
+
+		<div class="p3">
+		<p>
+			Quand je pense a l'architecture, des images remontent en moi. Beaucoup de ces images sont
+			en rapport avec ma formation et mon travail d'architecte. Elles contiennent le savoir sur 
+			l'architecture que j'ai pu accumuler au cours du temps.Dautres evoquent mon enfance. Je me rappelle
+			le temps ou je faisais l'experience de l'architecture sans y reflechir. Je crois sentir
+			encore dans ma main une poignee de porte, une piece de metal arrondie comme le dos d'une cuillere ...<br>
+			<i><b>Penser L'architecture </b><br> Peter Zumthor </i>
+		</p>
+
+	</div>
+		
+
+
+
+
+
+</body>
+
+
+
+<footer>
+			<p> Cem Gunes, Copyright &copy 2017</p>
+</footer>
+
+
+</html>
